be363730caea3732e5e0fb53b2fa3585f1704f3e
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-07-06 14:28:01 +0100

    seeding applicant data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  const user = await prisma.applicant.create({
+    data: {
+      id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+      firstName: "John",
+      lastName: "Doe",
+      email: "john.doe@example.com",
+      phone: "1234567890",
+    },
+  });
+  console.log("User created:", user);
+}
+
 main()
   .then(async () => {
     await prisma.$disconnect();
